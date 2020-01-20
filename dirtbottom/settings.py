0ce22b158f2eb26ecd1edc771cec9a52ec409102
Jeremy <jeremyncapps@fornost.localdomain>
2020-01-19 21:57:46 -0500

    Update Settings

--- dirtbottom/settings.py
+++ dirtbottom/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'mygarden.apps.MygardenConfig',
+    'myGarden.myGarden',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
